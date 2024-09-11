894ed7db41466d8d13e71d881353764b411430ff
skipperr254 <skipperr254@gmail.com>
2024-09-11 14:44:23 +0300

    Random Color Generator: Initial commit and project set-up

---
+++ random-color/src/App.js
@@ -0,0 +1,10 @@
+import RandomColor from './components/RandomColor';
+import './App.css';
+
+function App() {
+  return (
+    <RandomColor />
+  );
+}
+
+export default App;
---
+++ random-color/src/components/RandomColor.jsx
@@ -0,0 +1,9 @@
+const RandomColor = () => {
+    return (
+        <div>
+            <h1>Random Color Generated</h1>
+        </div>
+    )
+}
+
+export default RandomColor
