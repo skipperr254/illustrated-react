46eb1284f551c1338c48b99fff3cefcf9d7ebe1d
skipperr254 <skipperr254@gmail.com>
2024-09-16 23:32:33 +0300

    Carousel: A new beginning

--- image-slider/src/components/Carousel.jsx
+++ image-slider/src/components/Carousel.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react'
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa'
+import './Carousel.css'
+import images from './data.js'
+
 const Carousel = () => {
+    const [active, setActive] = useState(0)
+
     return (
-        <div>
-            <h1>Carousel</h1>
+        <div className="carousel-container">
+            <h1>
+                Image Slider/Carousel
+            </h1>
+            <div className="carousel">
+                <img src={images[0].imageUrl} alt="" />
+            </div>
+            <button>
+                <FaArrowCircleLeft />
+            </button>
+            <button>
+                <FaArrowCircleRight />
+            </button>
         </div>
     )
 }
---
+++ image-slider/src/components/data.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        imageUrl: "https://picsum.photos/200/300",
+        alt: "Image 1"
+    },
+    {
+        imageUrl: "https://picsum.photos/200/300",
+        alt: "Image 2"
+    },
+    {
+        imageUrl: "https://picsum.photos/200/300",
+        alt: "Image 3"
+    },
+    {
+        imageUrl: "https://picsum.photos/200/300",
+        alt: "Image 4"
+    },
+]
