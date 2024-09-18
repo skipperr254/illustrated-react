01f5261911b92057727da7abab361674a06eada0
skipperr254 <skipperr254@gmail.com>
2024-09-18 12:09:59 +0300

    Carousel: Fool and final

--- image-slider/src/components/Carousel.jsx
+++ image-slider/src/components/Carousel.jsx
@@ -1,24 +1,35 @@
-import { useState } from 'react'
-import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa'
-import './Carousel.css'
+import { useState } from "react"
+import { BsArrowRightCircleFill, BsArrowLeftCircleFill } from "react-icons/bs"
 import images from './data.js'
+import './Carousel.css'
 
 const Carousel = () => {
     const [active, setActive] = useState(0)
 
+    const showPrev = () => {
+        setActive(prevActive => {
+            return prevActive <= 0 ? images.length - 1 : prevActive - 1
+        })
+    }
+
+    const showNext = () => {
+        setActive(prevActive => {
+            return prevActive >= images.length - 1 ? 0 : prevActive + 1
+        })
+    }
+
     return (
-        <div className="carousel-container">
-            <h1>
-                Image Slider/Carousel
-            </h1>
-            <div className="carousel">
-                <img src={images[0].imageUrl} alt="" />
-            </div>
-            <button>
-                <FaArrowCircleLeft />
+        <div className="carousel">
+            {
+                images.map((_, index) => (
+                    <img src={images[index].imageUrl} alt={images[index].alt} style={{ translate: `-${100 * active}%` }} />
+                ))
+            }
+            <button className="arrow arrow-left" onClick={showPrev}>
+                <BsArrowLeftCircleFill />
             </button>
-            <button>
-                <FaArrowCircleRight />
+            <button className="arrow arrow-right" onClick={showNext}>
+                <BsArrowRightCircleFill />
             </button>
         </div>
     )
--- image-slider/src/components/data.js
+++ image-slider/src/components/data.js
@@ -1,18 +1,18 @@
 export default [
     {
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/500/500?random=1",
         alt: "Image 1"
     },
     {
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/500/500?random=2",
         alt: "Image 2"
     },
     {
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/500/500?random=3",
         alt: "Image 3"
     },
     {
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/500/500?random=4",
         alt: "Image 4"
     },
 ]
